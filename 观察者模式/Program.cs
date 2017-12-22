b6963542ff169ec9d4671f7f11530fef53070517
xuzeyu <286513187@qq.com>
2017-12-22 16:42:33 +0800

    增加观察者模式
    增加观察者模式

--- 观察者模式/Program.cs
+++ 观察者模式/Program.cs
@@ -24,7 +24,102 @@ namespace 观察者模式
     {
         static void Main(string[] args)
         {
+            Subject subject = new Subject();
 
+            new HexaObserver(subject);
+            new OctalObserver(subject);
+            new BinaryObserver(subject);
+
+            Console.WriteLine("First state change: 15");
+            subject.SetState(15);
+            Console.WriteLine("Second state change: 10");
+            subject.SetState(10);
+
+            Console.ReadKey();
+        }
+    }
+
+    /// <summary>
+    /// 步骤 1 创建 Subject 类。
+    /// </summary>
+    public class Subject
+    {
+        private List<Observer> observers= new List<Observer>();
+        private int state;
+
+        public int GetState()
+        {
+            return state;
+        }
+
+        public void SetState(int state)
+        {
+            this.state = state;
+            NotifyAllObservers();
+        }
+
+        public void Attach(Observer observer)
+        {
+            observers.Add(observer);
+        }
+
+        public void NotifyAllObservers()
+        {
+            foreach (Observer observer in observers)
+            {
+                observer.Update();
+            }
+        }
+    }
+
+    /// <summary>
+    /// 步骤 2 创建 Observer 类。
+    /// </summary>
+    public abstract class Observer
+    {
+        protected Subject subject;
+        public abstract void Update();
+    }
+
+    /// <summary>
+    /// 步骤 3 创建实体观察者类.
+    /// </summary>
+    public class BinaryObserver : Observer
+    {
+        public BinaryObserver(Subject subject)
+        {
+            this.subject = subject;
+            this.subject.Attach(this);
+        }
+        public override void Update()
+        {
+            Console.WriteLine("Binary String: " + Convert.ToString(subject.GetState(),2));
+        }
+    }
+
+    public class OctalObserver : Observer
+    {
+        public OctalObserver(Subject subject)
+        {
+            this.subject = subject;
+            this.subject.Attach(this);
+        }
+        public override void Update()
+        {
+            Console.WriteLine("Octal String: " + Convert.ToString(subject.GetState(), 8));
+        }
+    }
+
+    public class HexaObserver : Observer
+    {
+        public HexaObserver(Subject subject)
+        {
+            this.subject = subject;
+            this.subject.Attach(this);
+        }
+        public override void Update()
+        {
+            Console.WriteLine("Hex String: " + Convert.ToString(subject.GetState(), 16).ToUpper());
         }
     }
 }
