fc520b6f183cd94416e79fa5ec70f9ef7830985a
xuzeyu <286513187@qq.com>
2018-04-28 20:42:44 +0800

    增加状态模式
    增加状态模式

---
+++ 状态模式/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace 状态模式
+{
+    //    步骤 1
+    //创建一个接口。
+    public interface State
+    {
+        void doAction(Context context);
+    }
+
+    //    步骤 2
+    //创建实现接口的实体类。
+    public class StartState : State
+    {
+        public void doAction(Context context)
+        {
+            Console.WriteLine("Player is in start state");
+            context.setState(this);
+        }
+
+        public override String ToString()
+        {
+            return "Start State";
+        }
+    }
+
+    public class StopState : State
+    {
+        public void doAction(Context context)
+        {
+            Console.WriteLine("Player is in stop state");
+            context.setState(this);
+        }
+
+        public override String ToString()
+        {
+            return "Stop State";
+        }
+    }
+
+    //    步骤 3
+    //创建 Context 类。
+    public class Context
+    {
+        private State state;
+
+        public Context()
+        {
+            state = null;
+        }
+
+        public void setState(State state)
+        {
+            this.state = state;
+        }
+
+        public State getState()
+        {
+            return state;
+        }
+    }
+
+    //    步骤 4
+    //使用 Context 来查看当状态 State 改变时的行为变化。
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Context context = new Context();
+
+            StartState startState = new StartState();
+            startState.doAction(context);
+            Console.WriteLine(context.getState().ToString());
+
+            StopState stopState = new StopState();
+            stopState.doAction(context);
+
+            Console.WriteLine(context.getState().ToString());
+            Console.ReadKey();
+        }
+    }
+}
